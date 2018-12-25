95020bafb20fa31bbcbe13e6a426cdf0ed2e00a6
senthilkumar-r <r.senthilkumar@outlook.com>
2018-12-26 01:57:30 +0530

    update

--- app.js
+++ app.js
@@ -3,32 +3,54 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const app = express();
-
+const events = [];
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
-  schema: buildSchema(`
-    type RootQuery  {
-      events:[String!]!
-    }
-    type RootMutation{
-      createEvent(name: String): String
-    }
-    schema{
-      query:RootQuery
-      mutation:RootMutation
-    }`),
-  rootValue: {
-    events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event{
+          _id:ID!
+          title:String!
+          description:String!
+          price:Float!
+          date:String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        type RootQuery  {
+          events:[Event!]!
+        }
+        type RootMutation{
+          createEvent(eventInput: EventInput): Event
+        }
+        schema{
+          query:RootQuery
+          mutation:RootMutation
+        }`),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
     },
-    createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
-    }
-  },
-  graphiql: true
-})
+    graphiql: true
+  })
 );
 
 app.get("/", (req, res, next) => {
